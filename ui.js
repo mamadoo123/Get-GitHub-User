5c4d3d01d6d1ea2f6adc1b429c6bf94293b5bc05
mohamed khamis <mklhs66662000@gmail.com>
2019-01-20 13:57:06 +0200

    added the show alert utility and clear alert utility functions

--- ui.js
+++ ui.js
@@ -1,6 +1,8 @@
 class UI {
     constructor(){
         this.profile = document.getElementById('profile');
+        this.searchContainer = document.querySelector('.searchContainer');
+        this.search = document.querySelector('.search')
     }
 
     showProfile(userProfile){
@@ -32,5 +34,29 @@ class UI {
         <h3 class="page-heading mb-3">Latest Repos</h3>
         <div id="repos"></div>
         `
+    }
+
+    clearAllProfiles(){
+        this.profile.innerHTML = '';
+    }
+
+    showAlert(message){
+        // clear any remaining alert
+        this.clearAlert();
+        //create a div with text message
+        const div = document.createElement('div');
+        div.className='alert alert-danger';
+        div.appendChild(document.createTextNode(message));
+        // append the newly created div to the search container and before the search box
+        this.searchContainer.insertBefore(div, this.search);
+        // let the alert disappear afetr 3 sec
+        setTimeout(()=>{div.remove()}, 3000)
+    }
+
+            // clearAlert function check if an alert exists and remove it
+    clearAlert(){
+        if(this.searchContainer.firstElementChild.className ==="alert alert-danger"){
+            this.searchContainer.firstElementChild.remove();
+        }
     }
 }
