07dddaeb8048660552a7db2014a38edab2c8316c
mohamed khamis <mklhs66662000@gmail.com>
2019-01-20 15:57:02 +0200

    added a new function showRepos that render a list of repos items into the DOM

--- ui.js
+++ ui.js
@@ -35,6 +35,25 @@ class UI {
         <div id="repos"></div>
         `
     }
+    showRepos(repos){
+        let output = '';
+        repos.forEach(function(repo){
+            output += `
+            <div class="card card-body mb-2">
+                <div class="row">
+                    <div class="col-md-6">
+                        <a href="${repo.html}" target="_blank">${repo.name}</a>
+                    </div>
+                    <div class="col-md-6">
+                        <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+                        <span class="badge badge-secondary">Watchs: ${repo.watchers_count}</span>
+                        <span class="badge badge-success">Forks: ${repo.forms_count} </span>                       
+                    </div>
+                </div>
+            </div>`
+        });
+        document.getElementById('repos').innerHTML = output;
+    }
 
     clearAllProfiles(){
         this.profile.innerHTML = '';
