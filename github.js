91742d9abf7ef94c1898551f9a7a06bf212a9807
mohamed khamis <mklhs66662000@gmail.com>
2019-01-20 15:59:37 +0200

    added a new fetch to the getUser function to return an array repositories as well as the profile object

--- github.js
+++ github.js
@@ -1,15 +1,17 @@
 class GitHub{
     constructor(){
-        this.client_id = 'd9cdae56d758a7cad963';
-        this.client_secret = '0447679473c9ced4f63279e13b863193bb217d3e';
+        this.client_id = 'd9308aacf8b204d361fd';
+        this.client_secret = '84969aeef73956f4ec9e8716d1840532802bb81b';
+        this.number_of_repos = 5;
+        this.repos_sort = 'created: asc';//query string to format or sort the url
     }
 // Create The GET Request Function
     async getUser(username){
         const gitProfileResponse = await fetch(`https://api.github.com/users/${username}
         ?client_id=${this.client_id}&client_secret=${this.client_secret}/`);
         const profile = await gitProfileResponse.json();
-        return {
-            profile}
-
+        const gitUserRepos = await fetch(`https://api.github.com/users/${username}/repos?per_page=${this.number_of_repos}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const repos = await gitUserRepos.json();
+        return {profile, repos}
     }
 }
